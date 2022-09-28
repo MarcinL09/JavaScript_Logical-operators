8fd7d71e9f2cedbe4accdcc9440357dbb927d397
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-09-28 19:10:58 +0200

    added isTeenageAge function

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 function getName(name) {
     return name || 'Jan Kowalski';
 }
-const firstName = getName('Adam'); // 'Adam'
-const surname = getName(); // 'Jan Kowalski;
-console.log(firstName);
-console.log(surname);
+getName('Adam'); // 'Adam'
+getName(); // 'Jan Kowalski;
+console.log(getName());
+
+// Write the isTeenageAge function.
+
+function isTeenageAge(age) {
+    return  age >= 13 && age < 20;
+}
+
+let teenage = isTeenageAge(10); // false
+teenage = isTeenageAge(15); // true
+teenage = isTeenageAge(20); // false
+console.log(teenage);
