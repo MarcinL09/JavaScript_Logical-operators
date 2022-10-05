99048102a81eaec38fc3ca26f254517e9a57aacb
Marcin Laszczynski <mar.zczsal@gmail.com>
2022-10-05 19:19:11 +0200

    changed abbreviations to full words in 'rentalCarCost' function

--- index.js
+++ index.js
@@ -125,13 +125,13 @@ function _if(boolean, functionFirst, functionSecond) {
 // Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total.
 // Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 // Write a code that gives out the total amount for different days(d).
-function rentalCarCost(d) {
-    if (d >= 7) {
-        return d * 40 - 50;
-    } else if (d >= 3) {
-        return d * 40 - 20;
+function rentalCarCost(days) {
+    if (days >= 7) {
+        return days * 40 - 50;
+    } else if (days >= 3) {
+        return days * 40 - 20;
     }
-    return d * 40;
+    return days * 40;
 }
 
 const cost = rentalCarCost(2);
